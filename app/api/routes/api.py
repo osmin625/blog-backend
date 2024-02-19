24ec7822c6b2099ee29e22366c2af239a3dc8414
Oh seungmin <tmdals179@gmail.com>
2024-02-19 22:56:09 +0900

    feat: add user router

--- app/api/routes/api.py
+++ app/api/routes/api.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter
 
-from api.routes import predictor
+from api.routes import predictor, user
 
 router = APIRouter()
 router.include_router(predictor.router, tags=["predictor"], prefix="/v1")
+router.include_router(user.router, tags=["user"], prefix='/user')
